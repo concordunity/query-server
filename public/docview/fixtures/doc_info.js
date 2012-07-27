4b3dbe8411cd3d935b5ea91a962643951712abaf
zhouzhen <zhou1_zhen2@gmail.com>
2012-07-27 09:40:34 +0800

    １、针对插件的修改，将details下的list的功能进行了完善

---
+++ public/docview/fixtures/doc_info.js
@@ -0,0 +1,105 @@
+steal("jquery/dom/fixture", function() {
+    $.fixture(
+        "POST /search_form", function() {
+
+            var response = {
+                "doc_info":[{
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Beijing",
+                    "code":"2011111"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Shanghai",
+                    "code":"2011112"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Tianjing",
+                    "code":"2011114"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Chongqing",
+                    "code":"2011115"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"guangdong",
+                    "code":"2011116"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Shenzhen",
+                    "code":"2011117"
+                }
+                ,
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"xianggang",
+                    "code":"2011118"
+                }
+                ,
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"aomen",
+                    "code":"2011129"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Chongqing",
+                    "code":"2011125"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"guangdong",
+                    "code":"2011126"
+                },
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"Shenzhen",
+                    "code":"2011127"
+                }
+                ,
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"xianggang",
+                    "code":"2011128"
+                }
+                ,
+
+                {
+                    "doc_id":"220120121012010018",
+                    "Country":"China",
+                    "City":"aomen",
+                    "code":"2011129"
+                }
+                ]
+                
+            }
+            return response;
+        });
+});
