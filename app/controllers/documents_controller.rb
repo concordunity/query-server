72e4927bfb820ee592f268d5a4c4b54cf312f457
Weidong Shao <weidongshao@gmail.com>
2012-09-02 15:02:52 +0800

    update files

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -214,15 +214,15 @@ class DocumentsController < ApplicationController
   def query
     doc_id = params[:doc_id]
     special_doc_info = ""
-    # check user's privileges
-    if !current_user.can_view?(d)
-      render json: { :status => :error, :message => t('doc.not_authorized') }, :status => 403 
-      return
-    end
 
     @document = Document.find_by_doc_id(doc_id)
-
     if @document
+
+      # check user's privileges
+      if !current_user.can_view?(@document)
+        render json: { :status => :error, :message => t('doc.not_authorized') }, :status => 403 
+        return
+      end
       # Now check for user's org
       if @document.inquired &&  !(can? :inquired, Document)
         render json: { :status => :error, :message => t('doc.not_authorized') }, :status => 403 
