64f2c7ddd930557f6a94c106a2de4db0f63fe336
Weidong Shao <weidongshao@gmail.com>
2012-09-21 15:11:22 +0800

    Add export query ui

--- public/docview/ui/export_query/export_query.js
+++ public/docview/ui/export_query/export_query.js
@@ -6,12 +6,13 @@ steal(
 ).then(
     './views/init.ejs',
     'docview/ui/dmstable',
+    'docview/models',
     'docview/ui/queryform'
 ).then(function($) {
     $.Controller('Docview.Ui.Export_query', {}, {
         init : function() {
             this.element.html(this.view('init'));
-	    this.element.hide();
+
 	    var formParams = {
 		formClass: "stats_export",
 		legend: "出口删改单借出清单",
@@ -27,17 +28,19 @@ steal(
 						     this.proxy('handleError'));
 	},
 	handleData : function (data) {
-	    console.log(data);
+
 	    var table_options = {
+		file_name: "",
 		aaData: data.results,
 		col_def_path : "//docview/ui/export_query/views/",
 		aoColumns: [
 		    {"mDataProp" : "doc_id", mLabel : '单证号' },
 		    {"mDataProp" : "edc_date", mLabel : '借出日期' }
-		]
+		],
+		col_width: [0,0,0]
 	    };
-	    console.log("about to add control ", this.element.find('div.export_results'));
-	    this.element.find('div.export_results').docview_ui_dmstable({ table_options: table_options });
+
+	    this.element.find('.export_results').docview_ui_dmstable({ table_options: table_options });
 	},
 	handleError: function (jqstatus, error) {
 	},
