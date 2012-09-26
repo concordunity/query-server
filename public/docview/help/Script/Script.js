54bd86b42aa8d7cb47bcbacdffef1ee34427e4dc
Weidong Shao <weidongshao@gmail.com>
2012-09-26 16:30:39 +0800

    New Help files

---
+++ public/docview/help/Script/Script.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $('#backTop').mouseover(function () {
+        $(this).css('backgroundImage', 'url(Image/backTop_1.gif)');
+    }).mouseout(function () {
+        $(this).css('backgroundImage', 'url(Image/backTop_0.gif)');
+    }).click(function () {
+        $(this).blur();
+    });
+});
